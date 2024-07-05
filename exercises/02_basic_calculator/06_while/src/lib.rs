2e1666a5b3b590fa588462f78bc4e1e9a476b281
Nikhil Kamath <nikhil.kamath@appian.com>
2024-07-05 11:59:28 -0400

    Parts 1 & 2

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,13 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut output = 1;
+    let mut _n = n;
+    while _n > 0 {
+        output *= _n;
+        _n -= 1;
+    }
+    output
 }
 
 #[cfg(test)]
